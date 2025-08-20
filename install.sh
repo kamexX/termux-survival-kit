aea7f2a4ffd91e8a9f78db342d230a53b1f2526d
kamexX <61888847+kamexX@users.noreply.github.com>
2025-08-20 15:57:39 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Termux Survival Kit Installer
+# Installiert wichtige Unix-Tools und Development-Tools für Termux
+
+echo "=== Termux Survival Kit Installer ==="
+
+# Update & Upgrade Termux
+echo "[*] Updating packages..."
+pkg update -y && pkg upgrade -y
+
+# Liste der Pakete
+PACKAGES=(
+  zsh
+  bash
+  tmux
+  htop
+  git
+  vim
+  nano
+  python
+  nodejs
+  golang
+  openssh
+  wget
+  curl
+  nmap
+  net-tools
+  busybox
+  coreutils
+  findutils
+  tree
+  jq
+  rsync
+  mc
+)
+
+# Installation der Pakete
+for pkgname in "${PACKAGES[@]}"; do
+    echo "[*] Installing $pkgname..."
+    if pkg list-installed | grep -q "^$pkgname"; then
+        echo "[!] $pkgname is already installed, skipping."
+    else
+        pkg install -y $pkgname
+    fi
+done
+
+# Oh-My-Zsh Installation
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "[*] Installing Oh-My-Zsh..."
+    git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
+    cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
+else
+    echo "[!] Oh-My-Zsh already installed, skipping."
+fi
+
+echo "=== Installation completed! ==="
+echo "You may want to change your default shell to zsh with: chsh -s zsh"
